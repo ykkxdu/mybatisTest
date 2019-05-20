f0a42c8bc154b42eb457760d724f2cfbed9544ec
ykkxdu <ykk_xdu@163.com>
2019-05-20 09:11:48 +0800

    merge

--- src/main/java/com/bid/ykk/mapper/UserMapper.java
+++ src/main/java/com/bid/ykk/mapper/UserMapper.java
@@ -4,8 +4,6 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.bid.ykk.entity.User;
-
-import java.util.List;
 public interface UserMapper extends BaseMapper<User> {
     IPage<User> getUser(Page page);
 }
